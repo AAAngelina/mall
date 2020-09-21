e26599c230d9f2c4b1cfd71753c5190fd650e941
AAAngelina <1095275349@qq.com>
2020-09-21 21:17:50 +0800

    详情页-商品评论信息展示+时间格式化函数

--- src/common/utils.js
+++ src/common/utils.js
@@ -7,3 +7,29 @@ export function debounce(func,delay) {  /*refresh防抖函数,返回新的函数
     },delay)
   }
 }
+
+export function formatDate(date,fmt) {   /*时间格式化*/
+  /*1.获取年份*/
+  if(/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1,(date.getFullYear() + '').substr(4-RegExp.$1.length))
+  }
+
+  let o = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds()
+  }
+  for(let k in o) {
+    if(new RegExp(`(${k})`).test(fmt)) {
+      let str = o[k] + '';
+      fmt = fmt.replace(RegExp.$1,(RegExp.$1.length === 1) ? str : padLeftZero(str));
+    }
+  }
+  return fmt;
+}
+
+function padLeftZero(str) {  /*匹配两位*/
+  return ('00'+str).substr(str.length)
+}
