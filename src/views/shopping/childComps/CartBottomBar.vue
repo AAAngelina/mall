a9a1524f8134452207e1f32b5170671ea283294c
AAAngelina <1095275349@qq.com>
2020-09-24 10:00:35 +0800

    购物车底部结算框的封装

---
+++ src/views/shopping/childComps/CartBottomBar.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="bottom-bar">
+    <div class="check-content">
+      <check-button class="check-button"/>
+      <span>全选</span>
+    </div>
+    <div class="price">
+      合计:{{totalPrice}}
+    </div>
+    <div class="calculate">
+      去计算({{checkedLength}})
+    </div>
+  </div>
+</template>
+
+<script>
+  import CheckButton from "components/content/checkButton/CheckButton";
+
+  import { mapGetters } from 'vuex'
+
+  export default {
+    name: "CartBottomBar",
+    components: {
+      CheckButton
+    },
+    computed: {
+      ...mapGetters(['cartList']),
+      totalPrice() {
+        return '￥' + this.cartList.filter(item => {
+          return item.checked    /*过滤选中的商品*/
+        }).reduce((preValue,item) => {   /*汇总*/
+          return preValue + item.price * item.count
+        },0).toFixed(2)  /*保留两位小数*/
+      },
+      checkedLength() {
+        return this.cartList.filter(item => item.checked).length
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .bottom-bar {
+    height: 40px;
+    line-height: 40px;
+    background-color: #eeeeee;
+    position: relative;
+    display: flex;
+    font-size: 14px;
+  }
+  .check-content {
+    display: flex;
+    align-items: center;
+    margin-left: 10px;
+    width: 70px;
+  }
+  .check-button {
+    width: 20px;
+    height: 20px;
+    line-height: 20px;
+    margin-right: 5px;
+  }
+  .price {
+    margin-left: 20px;
+    flex: 1;
+  }
+  .calculate {
+    width: 80px;
+    text-align: center;
+    background-color: #ff51ea;
+    color: #f2f5f8;
+  }
+</style>
